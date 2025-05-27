2c8e827bb4362f4daf09ae3708685527e4476f95
Isaac <quiloisaac@gmail.com>
2025-05-28 02:05:55 +0800

    Fixes

--- src/main/java/com/brightminds/brightminds_backend/model/GameMode.java
+++ src/main/java/com/brightminds/brightminds_backend/model/GameMode.java
@@ -1,8 +1,6 @@
 package com.brightminds.brightminds_backend.model;
 
 public enum GameMode {
-    BALLOON,       // Pop balloons with correct answers, bonus for 3 in a row
-    TREASURE_HUNT,  // Unlock treasure chests, bonus for completing all questions correctly
     MATCHING,       // Match pairs of related items (MatchingGamePage)
     IMAGE_MULTIPLE_CHOICE, // Select the correct image (ImageMultipleChoiceGame)
     SORTING,         // Sort items into categories (LikasYamanGame)
