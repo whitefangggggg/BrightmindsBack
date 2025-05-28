efb0bf6869cb6d024e095433dd3dfca69f7e5a53
Isaac <quiloisaac@gmail.com>
2025-05-29 07:45:46 +0800

    Working Teacher Profile Img

--- src/main/java/com/brightminds/brightminds_backend/controller/TeacherController.java
+++ src/main/java/com/brightminds/brightminds_backend/controller/TeacherController.java
@@ -2,6 +2,7 @@ package com.brightminds.brightminds_backend.controller;
 
 import com.brightminds.brightminds_backend.model.Teacher;
 import com.brightminds.brightminds_backend.repository.TeacherRepository;
+import com.brightminds.brightminds_backend.dto.TeacherProfileUpdateDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,10 +13,16 @@ public class TeacherController {
     @Autowired
     private TeacherRepository teacherRepository;
 
-    @PutMapping("/{id}/profile-photo")
-    public ResponseEntity<Teacher> updateProfilePhoto(@PathVariable Long id, @RequestParam String profilePhoto) {
+    @PutMapping("/{id}/profile")
+    public ResponseEntity<Teacher> updateProfile(@PathVariable Long id, @RequestBody TeacherProfileUpdateDto profileUpdate) {
         Teacher teacher = teacherRepository.findById(id).orElseThrow();
-        teacher.setProfilePhoto(profilePhoto);
+        
+        // Update teacher fields
+        teacher.setFirstName(profileUpdate.getFirstName());
+        teacher.setLastName(profileUpdate.getLastName());
+        teacher.setEmail(profileUpdate.getEmail());
+        teacher.setProfilePhoto(profileUpdate.getProfilePhoto());
+        
         teacherRepository.save(teacher);
         return ResponseEntity.ok(teacher);
     }
---
+++ src/main/java/com/brightminds/brightminds_backend/dto/TeacherProfileUpdateDto.java
@@ -0,0 +1,41 @@
+package com.brightminds.brightminds_backend.dto;
+
+public class TeacherProfileUpdateDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String profilePhoto;
+
+    // Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+} 
